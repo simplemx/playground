b86e13505d3a5479ee9d358c1826b4b200c2b552
simplemx <simplemx@126.com>
2013-09-04 12:00:54 +0800

    add logout

--- startup/main.py
+++ startup/main.py
@@ -3,8 +3,15 @@ import tornado.web
 import os
 
 class IndexHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
     def get(self):
         self.render("template/index.html")
+
+class LogoutHandler(tornado.web.RequestHandler):
+    def post(self):
+        self.clear_cookie("user")
+        self.redirect("/")
 
 class LoginHandler(tornado.web.RequestHandler):
     def get(self):
@@ -13,17 +20,21 @@ class LoginHandler(tornado.web.RequestHandler):
         "post login form"
         name = self.get_argument("name")
         password = self.get_argument("password")
-        self.write("do login:" + name + " pwd:" + password)
+        self.set_secure_cookie("user", name)
+        self.redirect("/")
 
 settings = {
     "static_path" : os.path.join(os.path.dirname(__file__), "resources"),
     "debug" : True,
     "login_url" : "/login",
+    "cookie_secret" : "test",
+    "xsrf_cookies" : True,
 }
 application = tornado.web.Application([
     (r"/", IndexHandler),
     (r"/login", LoginHandler),
     (r"/doLogin", LoginHandler),
+    (r"/logout", LogoutHandler),
 ], **settings)
 
 if __name__ == "__main__":

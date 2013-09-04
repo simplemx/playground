bf3b69c1dd9a5526084a06d24797ff9cce1c8b11
simplemx <simplemx@126.com>
2013-09-04 11:19:31 +0800

    add simple login page

--- startup/main.py
+++ startup/main.py
@@ -9,6 +9,11 @@ class IndexHandler(tornado.web.RequestHandler):
 class LoginHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("template/login.html")
+    def post(self):
+        "post login form"
+        name = self.get_argument("name")
+        password = self.get_argument("password")
+        self.write("do login:" + name + " pwd:" + password)
 
 settings = {
     "static_path" : os.path.join(os.path.dirname(__file__), "resources"),
@@ -18,6 +23,7 @@ settings = {
 application = tornado.web.Application([
     (r"/", IndexHandler),
     (r"/login", LoginHandler),
+    (r"/doLogin", LoginHandler),
 ], **settings)
 
 if __name__ == "__main__":

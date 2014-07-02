d5df51f548f232c1ee92c637fe48b5eb77db82fe
unknown <simplemx@126.com>
2014-07-02 08:30:45 +0800

    change

--- enterprise/enterprise/handler/enterprise.py
+++ enterprise/enterprise/handler/enterprise.py
@@ -1,11 +1,27 @@
 #encoding=utf-8
 import tornado.web
 
-class IndexHandler(tornado.web.RequestHandler):
+class EnterpriseHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
+    def renderMsg(self, msg):
+        self.render("msg.html", msg = msg)
+
+class QueryHandler(EnterpriseHandler):
+    def get(self):
+        self.render("busi_query.html")
+    def post(self):
+        print self.get_argument("cond1")
+        print self.get_argument("cond2")
+        self.renderMsg("成功")
+
+class OperateHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("busi_oper.html")
 
 handlers = [
-    (r"/", IndexHandler),
+    (r"/", EnterpriseHandler),
+    (r"/query", QueryHandler),
+    (r"/oper", OperateHandler),
         ]
 

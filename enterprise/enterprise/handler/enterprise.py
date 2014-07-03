b249a51cf21b951df04e61af53433ba78a14b1ed
unknown <simplemx@126.com>
2014-07-03 09:19:38 +0800

    change url to restful

--- enterprise/enterprise/handler/enterprise.py
+++ enterprise/enterprise/handler/enterprise.py
@@ -28,13 +28,22 @@ class OperateHandler(tornado.web.RequestHandler):
                 "id" : "2"
                 }]
         self.render("busi_oper.html", services = services)
+
+class OperateUserInfoHandler(EnterpriseHandler):
+    def get(self):
+        service_id = self.get_argument("service-id")
+        self.render("busi_oper_userinfo.html", service_id = service_id)
     def post(self):
-        print self.get_argument("service-id")
-        self.render("busi_oper_confirm.html")
+        service_id = self.get_argument("service_id")
+        cond1 = self.get_argument("cond1")
+        cond2 = self.get_argument("cond2")
+        print ("%s %s %s" % (service_id, cond1, cond2))
+        self.render("busi_oper_result.html", result_msg = "成功")
 
 handlers = [
     (r"/", EnterpriseHandler),
-    (r"/query", QueryHandler),
-    (r"/oper", OperateHandler),
+    (r"/querybusi", QueryHandler),
+    (r"/operatebusi", OperateHandler),
+    (r"/operatebusi/userinfo", OperateUserInfoHandler),
         ]
 

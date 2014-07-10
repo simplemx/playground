912ea57df7b1064b8dd20e3be0992c7ed7dc44c4
unknown <simplemx@126.com>
2014-07-10 09:18:12 +0800

    change

--- enterprise/resources/js/app.js
+++ enterprise/resources/js/app.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.ajaxGet = function(url, containerSelector, success) {
+    $.ajaxHTML = function(url, containerSelector, success) {
         $(containerSelector).html("<div class='ui-loading'>正在努力加载中...</div>")
         $.get(url, "_ajax_mode=true", function(data){
             var append = true
@@ -11,9 +11,14 @@ $(function(){
     };    
 
     $("#ui-sider-menu").on("click touchend", "li", function(){
-        var link_tag = $("a", $(this)),
+        var $this = $(this),
+            link_tag = $("a", $this),
             href = link_tag.data("href");
+        $.ajaxHTML(href, "#ui-content") 
 
-        $.ajaxGet(href, "#ui-content") 
+        //remove li active class
+        $this.parent().children().removeClass("active")
+        //active li
+        $this.addClass("active")
     }) 
 })

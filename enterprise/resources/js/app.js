bdc94cb09432a88326151a32b84e2ababa648b7a
simplemx <simplemx@126.com>
2014-07-17 10:06:22 +0800

    add loading

--- enterprise/resources/js/app.js
+++ enterprise/resources/js/app.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.ajaxHTML = function(url, containerSelector, success) {
+    $.ajaxGetHTML = function(url, containerSelector, success) {
         $(containerSelector).html("<div class='ui-loading'>正在努力加载中...</div>")
         $.get(url, "_ajax_mode=true", function(data){
             var append = true
@@ -8,17 +8,30 @@ $(function(){
             }
             append && $(containerSelector).html(data)
         })
-    };    
+    }
 
     $("#ui-sider-menu").on("click touchend", "li", function(){
         var $this = $(this),
             link_tag = $("a", $this),
             href = link_tag.data("href");
-        $.ajaxHTML(href, "#ui-content") 
+        
+        $.ajaxGetHTML(href, "#ui-content") 
 
         //remove li active class
         $this.parent().children().removeClass("active")
         //active li
         $this.addClass("active")
-    }) 
+
+    })
+
+    $("#ui-content").on("click touchend", "form button[type=submit]", function(){
+        var $button = $(this)
+
+        $button.attr("disabled", "disabled")
+        $button.addClass("ui-loading")
+        var old_text = $button.text()
+        $button.text("提交ing")
+        
+        return false;
+    })
 })

599aa2cf1d268e71150871906a21348105b8ae50
simplemx <simplemx@126.com>
2014-09-23 17:18:00 +0800

    add console support for ie

--- enterprise/resources/js/app.js
+++ enterprise/resources/js/app.js
@@ -1,4 +1,26 @@
 $(function(){
+	(function() {
+		var method;
+		var noop = function () {};
+		var methods = [
+			'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+			'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+			'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+			'timeStamp', 'trace', 'warn'
+		];
+		var length = methods.length;
+		var console = (window.console = window.console || {});
+
+		while (length--) {
+			method = methods[length];
+
+			// Only stub undefined methods.
+			if (!console[method]) {
+				console[method] = noop;
+			}
+		}
+	}());
+	
 	if(typeof String.prototype.trim !== 'function') {
 		String.prototype.trim = function() {
 			return $.trim(this)
